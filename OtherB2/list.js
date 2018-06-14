96c58ca0801c7780999c7a9b57f4d72f4c8d00d5
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 14:38:04 +0530

    Adding List API endpoint

---
+++ OtherB2/list.js
@@ -0,0 +1,29 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	let response = {
+		"isBase64Encoded": 1,
+		"statusCode": 200,
+		"headers": {
+			"headerName": "headerValue"
+		},
+		"body": "..."
+	};
+
+	let itemType = (event.queryStringParameters && event.queryStringParameters.type) || "VEG";
+	console.log("Querying for ", itemType);
+
+	ddb.scan({
+		TableName: 'otherb2', ExpressionAttributeValues: { ':it': itemType }, FilterExpression: 'itemType = :it'
+	}, function (err, data) {
+		if (!err && data.Items) {
+			response.body = JSON.stringify(data.Items);
+		} else {
+			response.statusCode = 404;
+			response.body = "No items found";
+		}
+		callback(null, response);
+	});
+
+}
