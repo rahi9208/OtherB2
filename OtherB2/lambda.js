4b732319b13c6e819789490f7acc25a8cb2b2bbe
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 14:30:50 +0530

    Creating DynamoDB table

--- OtherB2/lambda.js
+++ OtherB2/lambda.js
@@ -1,7 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
-	console.log("Hello Bright Talk");
+	ddb.put({
+		TableName: 'otherb2',
+		Item: { 'itemCode': event.itemCode, 'itemName': event.itemName, 'itemPrice': event.itemPrice, 'itemType': event.itemType }
+	}, function (err, data) {
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, 'Successfully Persisted');
+		}
+	});
 
-	callback(null,'Successfully executed');
+
+	
 }
